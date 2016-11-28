a7ecdca4d4fa4793a1080c8fda494002211ce188
tungtv <tungtv@qsoft.com.vn>
2016-11-28 15:06:18 +0700

    up code

--- giaodien/right.php
+++ giaodien/right.php
@@ -8,25 +8,97 @@
                 <button class="btn btn-primary btn-style btn-search"><i class="fa fa-search"></i></button>
             </form>
         </div>
-        <div id="archives-2" class="widget widget_archive"><h3 class="widget_title">Archives</h3>
+        <div  class="widget widget_meta package-details-content">
+            <h3 class="title">Tin tức</h3>
             <ul>
-                <li><a href="http://demo.themeum.com/wordpress/wptravelkit/demo2/2016/07/">July 2016</a>
-                </li>
+                <li><a href="">July 2016</a></li>
+                <li><a href="">July 2016</a></li>
+                <li><a href="">July 2016</a></li>
+                <li><a href="">July 2016</a></li>
+                <li><a href="">July 2016</a></li><li><a href="">July 2016</a></li>
+                <li><a href="">July 2016</a></li><li><a href="">July 2016</a></li>
+                <li><a href="">July 2016</a></li>
+                <li><a href="">July 2016</a></li>
+                <li><a href="">July 2016</a></li>
+
             </ul>
         </div>
-        <div id="meta-2" class="widget widget_meta"><h3 class="widget_title">Meta</h3>
-            <ul>
-                <li><a href="http://demo.themeum.com/wordpress/wptravelkit/demo2/wp-login.php">Log in</a>
-                </li>
-                <li><a href="http://demo.themeum.com/wordpress/wptravelkit/demo2/feed/">Entries <abbr
-                            title="Really Simple Syndication">RSS</abbr></a></li>
-                <li><a href="http://demo.themeum.com/wordpress/wptravelkit/demo2/comments/feed/">Comments
-                        <abbr title="Really Simple Syndication">RSS</abbr></a></li>
-                <li><a href="https://wordpress.org/"
-                       title="Powered by WordPress, state-of-the-art semantic personal publishing platform.">WordPress.org</a>
-                </li>
-            </ul>
+        <div  class="widget widget_meta package-details-content">
+            <h3 class="title">Fanpage</h3>
+            <div class="fb-page" data-href="https://www.facebook.com/mixtourist.vietnam" style="width: 100% !important;"
+                 data-small-header="false" data-adapt-container-width="true" data-hide-cover="false"
+                 data-show-facepile="true">
+                <blockquote cite="https://www.facebook.com/mixtourist.vietnam" class="fb-xfbml-parse-ignore"><a
+                        href="https://www.facebook.com/mixtourist.vietnam">MixTourist Viet Nam</a></blockquote>
+            </div>
         </div>
+        <div  class="widget widget_archive package-details-content">
+            <h3 class="title">Thư viện ảnh</h3>
+            <div class="row margin-bottom photo-gallery-item package-details-content">
+
+                <div class="photo-gallery-items  col-sm-12 img_right">
+                    <div class="gallery-items-img"><a
+                            href="http://demo.themeum.com/wordpress/wptravelkit/demo2/wp-content/uploads/sites/2/2016/10/Image_11-1.jpg"
+                            class="plus-icon"><img
+                                src="http://demo.themeum.com/wordpress/wptravelkit/demo2/wp-content/uploads/sites/2/2016/10/Image_11-1-570x400.jpg"
+                                class="img-responsive" alt=""></a></div>
+                </div>
+                <div class="photo-gallery-items col-sm-6 col-md-6 img_right">
+                    <div class="gallery-items-img">
+                        <a href="http://demo.themeum.com/wordpress/wptravelkit/demo2/wp-content/uploads/sites/2/2016/07/131731.jpg"
+                           class="plus-icon"><img
+                                src="http://demo.themeum.com/wordpress/wptravelkit/demo2/wp-content/uploads/sites/2/2016/07/131731-570x400.jpg"
+                                class="img-responsive" alt="photo : "></a>
+                    </div><!--/.gallery-items-img-->
+                </div> <!--/.col-md-3-->
+
+                <div class="photo-gallery-items col-sm-6 col-md-6 img_right">
+                    <div class="gallery-items-img">
+                        <a href="http://demo.themeum.com/wordpress/wptravelkit/demo2/wp-content/uploads/sites/2/2016/07/1722.jpg"
+                           class="plus-icon"><img
+                                src="http://demo.themeum.com/wordpress/wptravelkit/demo2/wp-content/uploads/sites/2/2016/07/1722-570x400.jpg"
+                                class="img-responsive" alt="photo : "></a>
+                    </div><!--/.gallery-items-img-->
+                </div> <!--/.col-md-3-->
+
+                <div class="photo-gallery-items col-sm-6 col-md-6 img_right">
+                    <div class="gallery-items-img">
+                        <a href="http://demo.themeum.com/wordpress/wptravelkit/demo2/wp-content/uploads/sites/2/2016/07/15171.jpg"
+                           class="plus-icon"><img
+                                src="http://demo.themeum.com/wordpress/wptravelkit/demo2/wp-content/uploads/sites/2/2016/07/15171-570x400.jpg"
+                                class="img-responsive" alt="photo : "></a>
+                    </div><!--/.gallery-items-img-->
+                </div> <!--/.col-md-3-->
+
+                <div class="photo-gallery-items col-sm-6 col-md-6 img_right">
+                    <div class="gallery-items-img">
+                        <a href="http://demo.themeum.com/wordpress/wptravelkit/demo2/wp-content/uploads/sites/2/2016/07/15231.jpg"
+                           class="plus-icon"><img
+                                src="http://demo.themeum.com/wordpress/wptravelkit/demo2/wp-content/uploads/sites/2/2016/07/15231-570x400.jpg"
+                                class="img-responsive" alt="photo : "></a>
+                    </div><!--/.gallery-items-img-->
+                </div> <!--/.col-md-3-->
+
+                <div class="photo-gallery-items col-sm-6 col-md-6 img_right">
+                    <div class="gallery-items-img">
+                        <a href="http://demo.themeum.com/wordpress/wptravelkit/demo2/wp-content/uploads/sites/2/2016/07/15305.jpg"
+                           class="plus-icon"><img
+                                src="http://demo.themeum.com/wordpress/wptravelkit/demo2/wp-content/uploads/sites/2/2016/07/15305-570x400.jpg"
+                                class="img-responsive" alt="photo : "></a>
+                    </div><!--/.gallery-items-img-->
+                </div> <!--/.col-md-3-->
+
+                <div class="photo-gallery-items col-sm-6 col-md-6 img_right">
+                    <div class="gallery-items-img">
+                        <a href="http://demo.themeum.com/wordpress/wptravelkit/demo2/wp-content/uploads/sites/2/2016/07/47072.jpg"
+                           class="plus-icon"><img
+                                src="http://demo.themeum.com/wordpress/wptravelkit/demo2/wp-content/uploads/sites/2/2016/07/47072-570x400.jpg"
+                                class="img-responsive" alt="photo : "></a>
+                    </div><!--/.gallery-items-img-->
+                </div> <!--/.col-md-3-->
+            </div>
+        </div>
+
     </aside>
 </div> <!-- #sidebar -->
 </div> <!-- .row -->
